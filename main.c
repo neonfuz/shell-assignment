63e96379b80da74ad171d1b7e58e28ed580ec0ee
Sage Raflik <neonfuz@gmail.com>
2016-02-16 10:02:10 -0600

    Added output redirection support

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 
 static inline
 int is_whitespace(char c)
@@ -43,16 +44,19 @@ int main(int argc, char **argv)
 
         int args_len = parse_line(line, args);
 
-        if (args_len >= 3 && strcmp(">", args[args_len-2]) == 0 )
-            puts("Pipes not yet implemented...");
-
-        if (args_len > 0 && strcmp("exit", args[0]) == 0) {
+        if (args_len > 0 && strcmp("exit", args[0]) == 0)
             break;
-        }
 
         pid_t childPid = fork();
 
         if(childPid == 0) {
+            int fd = STDOUT_FILENO;
+
+            if (args_len >= 3 && strcmp(">", args[args_len-2]) == 0 ) {
+                close(fd);
+                fd = open(args[args_len-1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            }
+
             execvp(args[0], args);
             break;
         } else {
